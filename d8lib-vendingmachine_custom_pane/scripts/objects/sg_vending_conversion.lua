ef7d3bee65728fcd34ed01f6da241217a081016a
Darkcraft8 <90066646+Darkcraft8@users.noreply.github.com>
2023-12-11 17:01:33 +0100

    Update sg_vending_conversion.lua so that there no legal problem

--- d8lib-vendingmachine_custom_pane/scripts/objects/sg_vending_conversion.lua
+++ d8lib-vendingmachine_custom_pane/scripts/objects/sg_vending_conversion.lua
@@ -1,22 +1,14 @@
 require "/scripts/util.lua"
-
+require "/objects/sgvending/sgvending.lua"
+ShellguardInit = init
 function init()
-    --shellguard dev code. get interaction action and data from the object and overrride them with the one of the parentVendor
-  self.iAction = config.getParameter("iAction")
-  self.iData = config.getParameter("iData")
-  
-  local itemConfig = root.itemConfig(config.getParameter("parentVendor"))
-  self.iData.paneLayoutOverride = itemConfig.config.interactData.paneLayoutOverride
-  self.iData.buyFactor = itemConfig.config.interactData.buyFactor
-  self.iData.sellFactor = itemConfig.config.interactData.sellFactor
-  self.iData.items = itemConfig.config.interactData.items
-  
-  object.setInteractive(true)
+  --shellguard dev code. get interaction action and data from the object and overrride them with the one of the parentVendor
+  ShellguardInit()
 	-- patch code
   object.setConfigParameter("interactData", self.iData) -- set the interactData of the placed object to iData so that it can be used by the custom vending pane
 end
 
-function onInteraction(args) -- replaced entire onInteraction so that the custom vending pane is used instead
+function onInteraction(args) -- replaced entire onInteraction so that the custom vending pane is used instead of the vanilla OpenMerchantPane
   self.config = nil
   self.interactData = config.getParameter("interactData")
   if self.interactData.config == "/interface/windowconfig/merchant.config" then
